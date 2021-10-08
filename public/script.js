d978c6c6907347e1c1a5df1a19eecd7508b16f52
martijnkunstman <martijn@kunstman.nl>
2021-10-08 22:18:38 +0200

    ambient snesor

---
+++ public/script.js
@@ -0,0 +1,32 @@
+let console1 = document.getElementById("console1");
+let console2 = document.getElementById("console2");
+let console3 = document.getElementById("console3");
+let console4 = document.getElementById("console4");
+let console5 = document.getElementById("console5");
+
+console5.innerHTML ="ok";
+
+navigator.permissions.query({ name: 'ambient-light-sensor' }).then(result => {
+    if (result.state === 'denied') {
+        console1.innerHTML='Permission to use ambient light sensor is denied.';
+        console.log('Permission to use ambient light sensor is denied.');
+        return;
+    }
+
+    
+    const als = new AmbientLightSensor({frequency: 20});
+    als.addEventListener('activate', () => console2.innerHTML='Ready to measure EV.');
+    als.addEventListener('error', event => console3.innerHTML=`Error: ${event.error.name}`);
+    als.addEventListener('reading', () => {
+        // Defaut ISO value.
+        const ISO = 100;
+        // Incident-light calibration constant.
+        const C = 250;
+
+        let EV = Math.round(Math.log2((als.illuminance * ISO) / C));
+        console.log(`Exposure Value (EV) is: ${EV}`);
+        console4.innerHTML=`Exposure Value (EV) is: ${EV}`;
+    });
+
+    als.start();
+});
