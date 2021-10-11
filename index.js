8bb5e3806d151d86658d5036133885465c8a0d6e
martijnkunstman <martijn@kunstman.nl>
2021-10-11 14:07:58 +0200

    fileWrite

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
 
 app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/postdata', (req, res) => {
+    const encodedStr = req.body.postData.replace(/[\u00A0-\u9999<>\&]/g, function(i) {
+        return '&#'+i.charCodeAt(0)+';';
+     });
+    fs.appendFile('data.txt', encodedStr+"<br>", err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })
+    res.send({ "status": "ok" })
+})
+
+app.get('/data', (req, res) => {
+    fs.readFile('data.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.send({ "data": data })
+    })
+})
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- public/script.js
+++ public/script.js
@@ -1,33 +1,56 @@
+let textSubmit = document.getElementById("textSubmit");
+let textInput = document.getElementById("textInput");
+let textData = document.getElementById("textData");
+textSubmit.addEventListener("click", function () {
+
+    fetch('/postdata', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ postData: textInput.value })
+    }).then(res => res.json()).then(res => { console.log(res.status); getData() });
+
+    textInput.value = "";
+})
+
+function getData() {
+    fetch('/data')
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.data);
+            textData.innerHTML = res.data;
+        });
+}
+getData();
+setInterval(getData, 5000);
+
 let x = 0
 let y = 0;
 let z = 0;
 
-function changeAxis()
-{
-    document.getElementById("axizx").innerHTML = document.getElementById("xaxis").value;
-    document.getElementById("axizy").innerHTML = document.getElementById("yaxis").value;
-    document.getElementById("axizz").innerHTML = document.getElementById("zaxis").value;
-}
-
-
 let acl = new Accelerometer({ frequency: 60 });
 acl.addEventListener('reading', () => {
-    
+
     let gravity = 9.8;
     let coefficient = 0.05;
+
+    x = acl.x;
+    y = acl.y;
+    z = acl.z;
 
     x = x + ((acl.x - x) * coefficient);
     y = y + ((acl.y - y) * coefficient);
     z = z + ((acl.z - z) * coefficient);
-    
 
-    document.getElementById("myRangeX").value = Math.round(x * 10) / 10/gravity;
-    document.getElementById("myRangeY").value = Math.round(y * 10) / 10/gravity;
-    document.getElementById("myRangeZ").value = Math.round(z * 10) / 10/gravity;
-   
-    document.getElementById("vx").innerHTML = Math.round(x/gravity*10)/10;
-    document.getElementById("vy").innerHTML = Math.round(y/gravity*10)/10;
-    document.getElementById("vz").innerHTML = Math.round(z/gravity*10)/10;
+    document.getElementById("myRangeX").value = Math.round(x * 10) / 10 / gravity;
+    document.getElementById("myRangeY").value = Math.round(y * 10) / 10 / gravity;
+    document.getElementById("myRangeZ").value = Math.round(z * 10) / 10 / gravity;
+
+    document.getElementById("myRangeXValue").innerHTML = Math.round(x * 10) / 10 / gravity;
+    document.getElementById("myRangeYValue").innerHTML = Math.round(y * 10) / 10 / gravity;
+    document.getElementById("myRangeZValue").innerHTML = Math.round(z * 10) / 10 / gravity;
 
 });
 
@@ -44,63 +67,63 @@ init();
 animate();
 
 function init() {
-  // Setup
-  renderer = new THREE.WebGLRenderer({canvas: document.querySelector("#canvas")});
+    // Setup
+    renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#canvas") });
 
-  // Make and setup a camera.
-  camera = new THREE.PerspectiveCamera(70, 1, 1, 1000);
-  camera.position.z = 400;
+    // Make and setup a camera.
+    camera = new THREE.PerspectiveCamera(70, 1, 1, 1000);
+    camera.position.z = 400;
 
-  // Make a scene
-  scene = new THREE.Scene();
+    // Make a scene
+    scene = new THREE.Scene();
 
-  // Make a cube.
-  var geometry = new THREE.BoxGeometry(200, 200, 200);
+    // Make a cube.
+    var geometry = new THREE.BoxGeometry(200, 200, 200);
 
-  // Make a material
-  var material = new THREE.MeshPhongMaterial({
-    ambient: 0x00505f,
-    color: 0xFFC90,
-    specular: 0xffffff,
-    shininess: 50,
-    shading: THREE.SmoothShading
-  });
+    // Make a material
+    var material = new THREE.MeshPhongMaterial({
+        ambient: 0x00505f,
+        color: 0xFFC90,
+        specular: 0xffffff,
+        shininess: 50,
+        shading: THREE.SmoothShading
+    });
 
-  // Create a mesh based on the geometry and material
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+    // Create a mesh based on the geometry and material
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-  // Add 2 lights.
-  var light1 = new THREE.PointLight(0xff0040, 2, 0);
-  light1.position.set(200, 100, 300);
-  scene.add(light1);
+    // Add 2 lights.
+    var light1 = new THREE.PointLight(0xff0040, 2, 0);
+    light1.position.set(200, 100, 300);
+    scene.add(light1);
 
-  var light2 = new THREE.PointLight(0x0040ff, 2, 0);
-  light2.position.set(-200, 100, 300);
-  scene.add(light2);
+    var light2 = new THREE.PointLight(0x0040ff, 2, 0);
+    light2.position.set(-200, 100, 300);
+    scene.add(light2);
 
 }
 
 function resize() {
-  var width = renderer.domElement.clientWidth;
-  var height = renderer.domElement.clientHeight;
-  if (renderer.domElement.width !== width || renderer.domElement.height !== height) {
-    renderer.setSize(width, height, false);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-  }
+    var width = renderer.domElement.clientWidth;
+    var height = renderer.domElement.clientHeight;
+    if (renderer.domElement.width !== width || renderer.domElement.height !== height) {
+        renderer.setSize(width, height, false);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    }
 }
 
 function animate() {
-  resize();
-  //mesh.rotation.x += 0.005;
-  //mesh.rotation.y += 0.01;
+    resize();
+    //mesh.rotation.x += 0.005;
+    //mesh.rotation.y += 0.01;
 
-  mesh.rotation.x = document.getElementById('myRangeX').value*Math.PI;
-  mesh.rotation.y = document.getElementById('myRangeY').value*Math.PI;
-  mesh.rotation.z = document.getElementById('myRangeZ').value*Math.PI;
+    mesh.rotation.x = document.getElementById('myRangeX').value * Math.PI;
+    mesh.rotation.y = document.getElementById('myRangeY').value * Math.PI;
+    mesh.rotation.z = document.getElementById('myRangeZ').value * Math.PI;
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
