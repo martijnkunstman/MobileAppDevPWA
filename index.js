1b173c9070fa72b35c3b901ad2c3bc1264aa6680
Martijn Kunstman <martijn@kunstman.nl>
2023-03-31 15:58:36 +0200

    parth

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 const fs = require('fs');
+const path = require('path')
 
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.post('/postdata', (req, res) => {
